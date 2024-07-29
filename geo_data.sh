0e4a14c6c5b7ff90e83268c95a3837558eba634a
Charlee Cobb <148910743+Char-Cobb@users.noreply.github.com>
2024-07-29 19:09:23 -0400

    Create geo_data.sh
    shell script to run sbatch

---
+++ geo_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mem=64GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+
+#myscript.sh
+
+if test $# -lt 2; then
+    echo "usage: $0 GSEACC ROOTPATH"
+    exit 1
+fi
+arg1=$1
+arg2=$2
+
+# copy input locally
+# cp $arg1 /hpfs/userws/cobbc07/NCBI_files/test_rnaseq_counts.txt
+
+python geo_data.py $arg1 $arg2
